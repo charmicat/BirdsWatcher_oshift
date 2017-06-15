3e22befc0fa67e2dfa9e477b0429639610fa1c62
Luiza <charmicat@gmail.com>
2017-06-15 02:37:26 +0100

    os

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='BirdsWatcher',
+    version='1.0',
+    packages=['vag'],
+    url='https://birdswatcher-vag.rhcloud.com/',
+    license='',
+    author='Luiza Utsch',
+    author_email='',
+    description='',
+    install_requires=
+    [
+        'Flask==0.12',
+        'Flask_OAuthlib=0.9.3'
+    ]
+)
